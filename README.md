5ac9158b2d149ebb42579ad52d3c27840afdd27b
olalekan <lekanmojibola@gmail.com>
2024-11-01 06:25:49 +0100

    redis

---
+++ README.md
@@ -0,0 +1,8 @@
+## FILES MANAGER
+### ALX JAVASCRIPT NODEJS ES6 NOSQL MONGODB EXPRESSJS
+
+### OVERVIEW
+This project is about managing various format of files ranging from text, images, videos and several other format of files. 
+
+### AUTHOR
+This project is written by Mojibola Olalekan
---
+++ utils/redis.js
@@ -0,0 +1,47 @@
+const redis = require('redis');
+
+class redisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => { console.log('this is the error', err); });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((res, rej) => {
+      this.client.get(key, (err, val) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(val);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((res, rej) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) {
+          rej(err);
+        } else { res(); }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((res, rej) => {
+      this.client.del(key, (err) => {
+        if (err) {
+          rej(err);
+        } else { res(); }
+      });
+    });
+  }
+}
+
+module.exports = new redisClient();
